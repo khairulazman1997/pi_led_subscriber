f47c90b110aad9e54034b6ec57f0f7f3d98c7ed3
Khairul <e0309879@u.nus.edu>
2021-03-11 14:03:24 +0800

    feat: Adds MQTT support the LED strips

--- python/examples/subscribe.py
+++ python/examples/subscribe.py
@@ -5,9 +5,13 @@
 # Direct port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
 
+# LED imports
 import time
 from rpi_ws281x import *
 import argparse
+
+#MQTT imports
+import paho.mqtt.client as mqtt
 
 # LED strip configuration:
 LED_COUNT      = 300      # Number of LED pixels.
@@ -19,9 +23,8 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+# --------------------------------------------LED Functions---------------------------------------------------------------------
 
-
-# Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
@@ -78,38 +81,44 @@ def theaterChaseRainbow(strip, wait_ms=50):
             for i in range(0, strip.numPixels(), 3):
                 strip.setPixelColor(i+q, 0)
 
-# Main program logic follows:
-if __name__ == '__main__':
-    # Process arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()
 
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
+# --------------------------------------------Subscriber Functions---------------------------------------------------------------------
 
-    print ('Press Ctrl-C to quit.')
-    if not args.clear:
-        print('Use "-c" argument to clear LEDs on exit')
+def on_connect(client, userdata, rc):
+    print("Connected with rc: " + str(rc))
+    client.subscribe("codomo/led")
 
-    try:
+def on_message(client, userdata, message):
 
-        while True:
-            print ('Color wipe animations.')
-            colorWipe(strip, Color(255, 0, 0))  # Red wipe
-            colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-            colorWipe(strip, Color(0, 0, 255))  # Green wipe
-            print ('Theater chase animations.')
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-            theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-            print ('Rainbow animations.')
-            rainbow(strip)
-            rainbowCycle(strip)
-            theaterChaseRainbow(strip)
+    if "red" in message.payload:
+        colorWipe(strip, Color(255, 0, 0))
+        return
 
-    except KeyboardInterrupt:
-        if args.clear:
-            colorWipe(strip, Color(0,0,0), 10)
+    if "green" in message.payload:
+        colorWipe(strip, Color(0, 255, 0))
+        return
+    
+    if "blue" in message.payload:
+        colorWipe(strip, Color(0, 0, 255))
+        return
+    
+    if "theaterChase" in message.payload:
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+
+    if "rainbow" in message.payload:
+        rainbow(strip)
+
+#----------------------------------------------LED Setup------------------------------------------------------------------------
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+#----------------------------------------------MQTT Setup------------------------------------------------------------------------
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("led.codomo.com", 1883, 60)
+
+client.loop_forever()
